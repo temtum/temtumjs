45c45be9ec9a094791e26bff1bab81616115c42e
Anton Shnip <anton.shnip@effective-soft.com>
2022-05-16 11:59:24 +0300

    feature: add new wallet method to get token for data creation

--- src/wallet.js
+++ src/wallet.js
@@ -2,6 +2,8 @@ const request = require('request-promise-native');
 const crypto = require('crypto');
 const secp256k1 = require('secp256k1');
 
+const Transaction = require('./transaction');
+
 class Wallet {
   static generateKeyPair() {
     const privateKey = crypto.randomBytes(32);
@@ -15,6 +17,35 @@ class Wallet {
 
   constructor(url) {
     this.url = url;
+  }
+
+  getSignatureForTokenCreation(address, privateKey) {
+    const timestamp = Transaction.getTimestamp();
+
+    const params = [
+      address,
+      timestamp
+    ];
+
+    const key = Transaction.sha256Hex(params.join(''));
+    const message = secp256k1.sign(
+      Buffer.from(key, 'hex'),
+      Buffer.from(privateKey, 'hex')
+    );
+
+    const signature = message.signature.toString('hex');
+
+    request({
+      method: 'POST',
+      url: `${this.url}/address/${address}/createToken`,
+      json: {signature}
+    })
+      .then(res => {
+        return res.token;
+      })
+      .catch(err => {
+        throw new Error(err);
+      })
   }
 
   sendTransaction(txHex) {
